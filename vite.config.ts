b1bffd9672772b4e7d7471b25274b2835f1135ce
hanikim <hanikim2057@gmail.com>
2024-12-02 13:30:11 +0900

    [feat] 불필요해진 vite proxy 설정 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/instagram-short": {
-        target: "https://api.instagram.com/oauth/access_token",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/instagram-short/, ""),
-      },
-      "/instagram-long": {
-        target: "https://graph.instagram.com/access_token",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/instagram-long/, ""),
-      },
-    },
-  },
 });
